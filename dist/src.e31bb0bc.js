f28189303630b655ddc9b8f71420eb12599942ce
valentina <valentinamarenda1@gmail.com>
2021-11-25 15:07:38 -0300

    cuarto commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -302,7 +302,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64068" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56740" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
