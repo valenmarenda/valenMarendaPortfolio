409aa3bb89767993c89526ffc12d5b88df6660f1
valentina <valentinamarenda1@gmail.com>
2021-11-25 16:13:12 -0300

    sexto commit

--- dist/anime.min.cfcbf369.js
+++ dist/anime.min.cfcbf369.js
@@ -1186,7 +1186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56740" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55876" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
